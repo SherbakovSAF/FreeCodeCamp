5477957d5d3470c65f71180b11ad350506a12e69
SherbakovSAF <sherbakovsaf@gmail.com>
2022-09-17 17:16:03 +0300

    Lesson 31-80 Complite

--- JS/LessonJS.js
+++ JS/LessonJS.js
@@ -1,3 +1,583 @@
+// Lesson 80
+// Setup
+// function abTest(a, b) {
+//      // Only change code below this line
+//    if (a < 0 || b <0) {
+//      return undefined;
+//    }
+   
+   
+//      // Only change code above this line
+   
+//      return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+//    }
+   
+//    abTest(2,2);
+// -----------------------------------------------------------------
+// Lesson 79
+// function isLess(a, b) {
+//      // Only change code below this line
+//        return a < b;
+//      // Only change code above this line
+//    }
+   
+//    isLess(10, 15);
+// -----------------------------------------------------------------
+// Lesson 78
+// function chainToSwitch(val) {
+//      let answer = "";
+//      // Only change code below this line
+//    switch (val) {
+//      case "bob":
+//      answer = "Marley";
+//      break;
+//      case 42:
+//      answer = "The Answer";
+//      break;
+//      case 1:
+//      answer = "There is no #1";
+//      break;
+//      case 99:
+//      answer = "Missed me by this much!";
+//      break;
+//      case 7:
+//      answer = "Ate Nine";
+//      break;
+   
+//    }
+//      // Only change code above this line
+//      return answer;
+//    }
+   
+//    chainToSwitch(7);
+// -----------------------------------------------------------------
+// Lesson 77
+// function sequentialSizes(val) {
+//      let answer = "";
+//      // Only change code below this line
+   
+//    switch (val) {
+//      case 1:
+//      case 2:
+//      case 3:
+//        answer = "Low";
+//        break;
+//      case 4:
+//      case 5:
+//      case 6:
+//      answer = "Mid";
+//        break;
+//      case 7:
+//      case 8:
+//      case 9:
+//      answer = "High";
+//        break;
+//    }
+   
+//      // Only change code above this line
+//      return answer;
+//    }
+   
+//    sequentialSizes(1);
+// -----------------------------------------------------------------
+// Lesson 76
+// function switchOfStuff(val) {
+//      let answer = "";
+//      // Only change code below this line
+//    switch (val) {
+//      case 'a':
+//        answer = 'apple';
+//        break;
+//      case 'b':
+//        answer = 'bird';
+//        break;
+//    case 'c':
+//        answer = 'cat';
+//        break;
+//      default:
+//        answer = 'stuff';
+//        break;
+//    }
+   
+   
+//      // Only change code above this line
+//      return answer;
+//    }
+   
+//    switchOfStuff(1);
+// -----------------------------------------------------------------
+// Lesson 75
+// function caseInSwitch(val) {
+//      let answer = "";
+//      // Only change code below this line
+//    switch(val){
+//      case 1:
+//      answer = "alpha";
+//      break
+//      case 2:
+//      answer = "beta";
+//      break
+//      case 3:
+//      answer = "gamma";
+//      break
+//      case 4:
+//      answer = "delta";
+//      break;
+//    }
+   
+   
+//      // Only change code above this line
+//      return answer;
+//    }
+   
+//    caseInSwitch(1);
+// -----------------------------------------------------------------
+// Lesson 74
+// const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+// function golfScore(par, strokes) {
+//   // Only change code below this line
+// if (strokes == 1) {
+//   return names[0]
+// } else if (strokes <= par - 2){
+// return names[1]
+// } else if (strokes <= par - 1){
+// return names[2]
+// } 
+// else if (strokes == par){
+// return names[3]
+// } else if (strokes == par + 1){
+// return names[4]
+// } 
+// else if (strokes == par + 2){
+// return names[5]
+// } else if (strokes >= par + 3){
+// return names[6]
+// } 
+
+//   return "Change Me";
+//   // Only change code above this line
+// }
+
+// golfScore(5, 4);
+// -----------------------------------------------------------------
+// Lesson 73
+// function testSize(num) {
+//      // Only change code below this line
+//      if (num < 5) {
+//      return "Tiny"
+//    } else if (num < 10) {
+//      return "Small"
+//    } else if (num < 15) {
+//      return "Medium"
+//    }
+//    else if (num < 20) {
+//      return "Large"
+//    }
+//    else {
+//      return "Huge"
+//    }
+//      // Only change code above this line
+//    }
+   
+//    testSize(7);
+// -----------------------------------------------------------------
+// Lesson 72
+// function orderMyLogic(val) {
+//      if (val < 5) {
+//            return "Less than 5";
+//      } else if (val < 10) {
+   
+//        return "Less than 10";
+//      } else {
+//        return "Greater than or equal to 10";
+//      }
+//    }
+   
+//    orderMyLogic(7);
+// -----------------------------------------------------------------
+// Lesson 71
+// function testElseIf(val) {
+//      if (val > 10) {
+//        return "Greater than 10";
+//      } else if (val < 5) {
+//        return "Smaller than 5";
+//      } else {
+//        return "Between 5 and 10";
+//      }
+//    }
+     
+//      testElseIf(7)
+// -----------------------------------------------------------------
+// Lesson 70
+// function testElse(val) {
+//      let result = "";
+//      // Only change code below this line
+   
+//      if (val > 5) {
+//        result = "Bigger than 5";
+//      } else {
+//    result = "5 or Smaller";
+//      }
+//      // Only change code above this line
+//      return result;
+//    }
+   
+//    testElse(4);
+// -----------------------------------------------------------------
+// Lesson 69
+// function testLogicalOr(val) {
+//      // Only change code below this line
+   
+//      if (val < 10 || val > 20) {
+//        return "Outside";
+//      }
+   
+//      // Only change code above this line
+//      return "Inside";
+//    }
+   
+//    testLogicalOr(15);
+// -----------------------------------------------------------------
+// Lesson 68
+// function testLogicalAnd(val) {
+//      // Only change code below this line
+   
+//      if (val <= 50 && val >=25) {
+//          return "Yes";
+//      }
+   
+//      // Only change code above this line
+//      return "No";
+//    }
+   
+//    testLogicalAnd(10);
+// -----------------------------------------------------------------
+// Lesson 67
+// function testLessOrEqual(val) {
+//      if (val <= 12) {  // Change this line
+//        return "Smaller Than or Equal to 12";
+//      }
+   
+//      if (val <= 24) {  // Change this line
+//        return "Smaller Than or Equal to 24";
+//      }
+   
+//      return "More Than 24";
+//    }
+   
+//    testLessOrEqual(10);
+// -----------------------------------------------------------------
+// Lesson 66
+// function testLessThan(val) {
+//      if (val < 25) {  // Change this line
+//        return "Under 25";
+//      }
+   
+//      if (val < 55) {  // Change this line
+//        return "Under 55";
+//      }
+   
+//      return "55 or Over";
+//    }
+   
+//    testLessThan(10);
+// -----------------------------------------------------------------
+// Lesson 65
+// function testGreaterOrEqual(val) {
+//      if (val >= 20) {  // Change this line
+//        return "20 or Over";
+//      }
+   
+//      if (val >= 10) {  // Change this line
+//        return "10 or Over";
+//      }
+   
+//      return "Less than 10";
+//    }
+   
+//    testGreaterOrEqual(10);
+// -----------------------------------------------------------------
+// Lesson 64
+// function testGreaterThan(val) {
+//      if (val > 100) {  // Change this line
+//        return "Over 100";
+//      }
+   
+//      if (val > 10) {  // Change this line
+//        return "Over 10";
+//      }
+   
+//      return "10 or Under";
+//    }
+   
+//    testGreaterThan(10);
+// -----------------------------------------------------------------
+// Lesson 63
+// Setup
+// function testStrictNotEqual(val) {
+//      if (val !== 17) { // Change this line
+//        return "Not Equal";
+//      }
+//      return "Equal";
+//    }
+   
+//    testStrictNotEqual(10);
+// -----------------------------------------------------------------
+// Lesson 62
+// Setup
+// function testNotEqual(val) {
+//      if (val != 99) { // Change this line
+//        return "Not Equal";
+//      }
+//      return "Equal";
+//    }
+   
+//    testNotEqual(10);
+// -----------------------------------------------------------------
+// Lesson 61
+// Setup
+// function compareEquality(a, b) {
+//      if (a === b) { // Change this line
+//        return "Equal";
+//      }
+//      return "Not Equal";
+//    }
+   
+//    compareEquality(10, "10");
+// -----------------------------------------------------------------
+// Lesson 60
+// Setup
+// function testStrict(val) {
+//      if (val ===7) { // Change this line
+//        return "Equal";
+//      }
+//      return "Not Equal";
+//    }
+   
+//    testStrict(10);
+// -----------------------------------------------------------------
+// Lesson 59
+// Setup
+// function testEqual(val) {
+//      if (val == 12) { // Change this line
+//        return "Equal";
+//      }
+//      return "Not Equal";
+//    }
+   
+//    testEqual(10);
+// -----------------------------------------------------------------
+// Lesson 58
+// function trueOrFalse(wasThatTrue) {
+//      // Only change code below this line
+//    if(wasThatTrue) {
+//      return 'Yes, that was true'
+//    } else {
+//    return 'No, that was false'
+//    }
+//    }
+// -----------------------------------------------------------------
+// Lesson 57
+// function nextInLine(arr, item) {
+//      // Only change code below this line
+//      arr.push(item)
+//      let delItem = arr.shift() 
+//      return delItem;
+//      // Only change code above this line
+//    }
+   
+//    // Setup
+//    const testArr = [1, 2, 3, 4, 5];
+   
+//    // Display code
+//    console.log("Before: " + JSON.stringify(testArr));
+//    console.log(nextInLine(testArr, 6));
+//    console.log("After: " + JSON.stringify(testArr));
+// -----------------------------------------------------------------
+// Lesson 56
+// let processed = 0;
+// function processArg(num) {
+//   return (num + 3) / 5;
+// }
+// processed = processArg(7);
+// Only change code below this line
+// -----------------------------------------------------------------
+// Lesson 55
+// Setup
+// let sum = 0;
+// function addThree() {
+//   sum = sum + 3;
+// }
+// // Only change code below this line
+// function addFive() {
+//   sum += 5;
+// }
+// // Only change code above this line
+// addThree();
+// addFive();
+// -----------------------------------------------------------------
+// Lesson 54
+// Setup
+// const outerWear = "T-Shirt";
+
+// function myOutfit() {
+//   // Only change code below this line
+// const outerWear = "sweater";
+//   // Only change code above this line
+//   return outerWear;
+// }
+
+// myOutfit();
+// -----------------------------------------------------------------
+// Lesson 53
+// function myLocalScope() {
+//      // Only change code below this line
+//    let myVar = 1;
+//      console.log('inside myLocalScope', myVar);
+//    }
+//    myLocalScope();
+   
+//    // Run and check the console
+//    // myVar is not defined outside of myLocalScope
+//    console.log('outside myLocalScope', myVar);
+// -----------------------------------------------------------------
+// Lesson 52
+// Declare the myGlobal variable below this line
+// const myGlobal = 10;
+
+// function fun1() {
+//   // Assign 5 to oopsGlobal Here
+// oopsGlobal = 5
+// }
+
+// // Only change code above this line
+
+// function fun2() {
+//   var output = "";
+//   if (typeof myGlobal != "undefined") {
+//     output += "myGlobal: " + myGlobal;
+//   }
+//   if (typeof oopsGlobal != "undefined") {
+//     output += " oopsGlobal: " + oopsGlobal;
+//   }
+//   console.log(output);
+// }
+// -----------------------------------------------------------------
+// Lesson 51
+// function timesFive(num) {
+//      return num * 5;
+// }
+// -----------------------------------------------------------------
+// Lesson 50
+// function functionWithArgs(a, b) {
+//      console.log(a + b);
+//    }
+   
+//    functionWithArgs(5,1)
+// -----------------------------------------------------------------
+// Lesson 49
+// function reusableFunction() {
+//     console.log("Hi World");
+// }
+// reusableFunction();
+// -----------------------------------------------------------------
+// Lesson 48
+// const myList = [["Banana", 1], ["Manana", 2], ["Panana", 14], ["Aanana", 15], ["Ganana", 12]];
+// -----------------------------------------------------------------
+// Lesson 47
+// const myArray = [["John", 23], ["dog", 3]];
+// myArray.shift();
+// myArray.unshift(["Paul", 35]);
+// -----------------------------------------------------------------
+// Lesson 46
+// const myArray = [["John", 23], ["dog", 3]];
+// let removedFromMyArray = myArray.shift()
+// -----------------------------------------------------------------
+// Lesson 45
+// const myArray = [["John", 23], ["cat", 2]];
+// const removedFromMyArray = myArray.pop()
+// -----------------------------------------------------------------
+// Lesson 44
+// const myArray = [["John", 23], ["cat", 2]];
+// myArray.push(["dog", 3])
+// -----------------------------------------------------------------
+// Lesson 43
+// const myArray = [
+//      [1, 2, 3],
+//      [4, 5, 6],
+//      [7, 8, 9],
+//      [[10, 11, 12], 13, 14],
+//    ];
+//    const myData = myArray[2][1];
+// -----------------------------------------------------------------
+// Lesson 42
+// const myArray = [18, 64, 99];
+// myArray[0] = 45;
+// -----------------------------------------------------------------
+// Lesson 41
+// const myArray = [50, 60, 70];
+// let myData = myArray[0];
+// -----------------------------------------------------------------
+// Lesson 40
+// Only change code below this line
+// const myArray = [[]];
+// -----------------------------------------------------------------
+// Lesson 39
+// Only change code below this line
+// const myArray = ['sFdsfsd', 1];
+// -----------------------------------------------------------------
+// Lesson 38
+// const myNoun = "dog";
+// const myAdjective = "big";
+// const myVerb = "ran";
+// const myAdverb = "quickly";
+// Only change code below this line
+// const wordBlanks = myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb; // Change this line
+// Only change code above this line
+// -----------------------------------------------------------------
+// Lesson 37
+// const lastName = "Lovelace";
+// Only change code below this line
+// const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+// -----------------------------------------------------------------
+// Lesson 36
+// const lastName = "Lovelace";
+// // Only change code below this line
+// const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
+// -----------------------------------------------------------------
+// Lesson 35
+// const lastName = "Lovelace";
+// Only change code below this line
+// const thirdLetterOfLastName = lastName[2]; // Change this line
+// -----------------------------------------------------------------
+// Lesson 34
+// Setup
+// let myStr = "Jello World";
+// Only change code below this line
+// myStr = myStr = "Hello World";
+// Only change code above this line
+// -----------------------------------------------------------------
+// Lesson 33
+// Setup
+// let firstLetterOfLastName = "";
+// const lastName = "Lovelace";
+// Only change code below this line
+// firstLetterOfLastName = lastName[0]; // Change this line
+// -----------------------------------------------------------------
+// Lesson 32
+// Setup
+// let lastNameLength = 0;
+// const lastName = "Lovelace";
+// Only change code below this line
+// lastNameLength = lastName.length;v
+// -----------------------------------------------------------------
+// Lesson 31
+// Change code below this line
+// const someAdjective = "Hello World";
+// let myStr = "Learning to code is ";
+// myStr += someAdjective
+// -----------------------------------------------------------------
 // Lesson 30
 // Only change code below this line
 // const myName = "Sergey ";
@@ -146,4 +726,4 @@
 // Lesson 1
 // This is an in-line comment.
 // This is a
-// multi-line comment 
+// multi-line comment
