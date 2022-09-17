fdc84d0913b18abab0523d27e48a66730a9cbc6b
SherbakovSAF <sherbakovsaf@gmail.com>
2022-09-17 14:39:52 +0300

    Lesson 1-30 Complite

---
+++ JS/LessonJS.js
@@ -0,0 +1,149 @@
+// Lesson 30
+// Only change code below this line
+// const myName = "Sergey ";
+// const myStr = "My name is " + myName + "I am well!";
+// -----------------------------------------------------------------
+// Lesson 29
+// let myStr = 'This is the first sentence. ';
+// myStr += 'This is the second sentence.'
+// -----------------------------------------------------------------
+// Lesson 28
+// const myStr = "This is the start. " + "This is the end."; // Change this line
+// -----------------------------------------------------------------
+// Lesson 27
+// const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+// -----------------------------------------------------------------
+// Lesson 26
+// const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+// -----------------------------------------------------------------
+// Lesson 25
+// const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line'
+// -----------------------------------------------------------------
+// Lesson 24
+// let a = 48;
+// let b = 108;
+// let c = 33;
+// Only change code below this line
+// a /= 12;
+// b /= 4;
+// c /= 11;
+// -----------------------------------------------------------------
+// Lesson 23
+// let a = 5;
+// let b = 12;
+// let c = 4.6;
+// Only change code below this line
+// a *= 5;
+// b *= 3;
+// c *= 10;
+// -----------------------------------------------------------------
+// Lesson 22
+// let a = 11;
+// let b = 9;
+// let c = 3;
+// -----------------------------------------------------------------
+// Only change code below this line
+// a -= 6;
+// b -= 15;
+// c -= 1;
+// Lesson 21
+// let a = 3;
+// let b = 17;
+// let c = 12;
+// // Only change code below this line
+// a += 12;
+// b += 9;
+// c += 7;
+// -----------------------------------------------------------------
+// Lesson 20
+// const remainder = 11 % 3;
+// -----------------------------------------------------------------
+// Lesson 19
+// const quotient = 4.4 / 2.0; // Change this line
+// -----------------------------------------------------------------
+// Lesson 18
+// const product = 2.0 * 2.5;
+// -----------------------------------------------------------------
+// Lesson 17
+// const ourDecimal = 5.7;
+// Only change code below this line
+// const myDecimal = 5.7;
+// -----------------------------------------------------------------
+// Lesson 16
+// let myVar = 11;
+// Only change code below this line
+// myVar--
+// -----------------------------------------------------------------
+// Lesson 15
+// let myVar = 87;
+// Only change code below this line
+// myVar++
+// -----------------------------------------------------------------
+// Lesson 14
+// onst quotient = 66 / 33;
+// -----------------------------------------------------------------
+// Lesson 13
+// const product = 8 * 10;
+// -----------------------------------------------------------------
+// Lesson 12
+// const difference = 45 - 33;
+// -----------------------------------------------------------------
+// Lesson 11
+// const sum = 10 + 10;
+// -----------------------------------------------------------------
+// Lesson 10
+// const FCC = "freeCodeCamp"; // Change this line
+// let fact = "is cool!"; // Change this line
+// fact = "is awesome!";
+// console.log(FCC, fact); // Change this line
+// -----------------------------------------------------------------
+// Lesson 9
+// let catName = "Oliver";
+// let catSound = "Meow!";
+// -----------------------------------------------------------------
+// Lesson 8
+// Variable declarations
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
+// -----------------------------------------------------------------
+// Variable assignments
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
+// -----------------------------------------------------------------
+// Lesson 7
+// Only change code below this line
+// var a = 5;
+// var b = 10;
+// var c = "I am a";
+// Only change code above this line
+// -----------------------------------------------------------------
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
+// -----------------------------------------------------------------
+// Lesson 6
+// var myFirstName = "Sergey";
+// var myLastName = "Sherbakov"
+// -----------------------------------------------------------------
+// Lesson 5
+// var a = 9;
+// -----------------------------------------------------------------
+// Lesson 4
+// var a;
+// a = 7;
+// var b;
+// b = a;
+// -----------------------------------------------------------------
+// Lesson 3
+// var a;
+// a = 7;
+// -----------------------------------------------------------------
+// Lesson 2
+// var myName;
+// -----------------------------------------------------------------
+// Lesson 1
+// This is an in-line comment.
+// This is a
+// multi-line comment 
