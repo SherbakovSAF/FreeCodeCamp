99aa8b63469d71e76fc5cd6a2d859c98818cb69e
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-07 00:34:35 +0300

    TestMethod Lesson 0-5 complite

---
+++ JS/TestMethod.js
@@ -0,0 +1,25 @@
+// Lesson 5
+// let extractStr = "Extract the word 'coding' from this string.";
+// let codingRegex = /coding/; // Change this line
+// let result = extractStr.match(codingRegex); // Change this line
+// --------------------------------------------------------
+// Lesson 4
+// let myString = "freeCodeCamp";
+// let fccRegex = /freeCodeCamp/i; // Change this line
+// let result = fccRegex.test(myString);
+// --------------------------------------------------------
+// Lesson 3
+// let petString = "James has a pet cat.";
+// let petRegex = /dog|cat|bird|fish/; // Change this line
+// let result = petRegex.test(petString);
+// --------------------------------------------------------
+// Lesson 2
+// let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+// let waldoRegex = /Waldo/; // Change this line
+// let result = waldoRegex.test(waldoIsHiding);
+// --------------------------------------------------------
+// Lesson 1
+// let myString = "Hello, World!";
+// let myRegex = /Hello/;
+// let result = myRegex.test(myString);; // Change this line
+// --------------------------------------------------------
