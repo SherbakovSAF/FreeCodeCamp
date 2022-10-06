71606fb141285ea7a7b3fa8b258516fc18fd7420
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-06 11:52:37 +0300

    ES6 Lesson 11-17 complite

--- JS/ES6.js
+++ JS/ES6.js
@@ -1,3 +1,82 @@
+// Lesson 18
+
+// --------------------------------------------------------
+// Lesson 17
+// // Only change code below this line
+// class Vegetable{
+//      constructor(name){
+//    this.name = name
+//      }
+//    }
+//    // Only change code above this line
+   
+//    const carrot = new Vegetable('carrot');
+//    console.log(carrot.name); // Should display 'carrot'
+// --------------------------------------------------------
+// Lesson 16
+// const bicycle = {
+//      gear: 2,
+//      setGear(newGear) {
+//        this.gear = newGear;
+//      }
+//    };
+//    // Only change code above this line
+//    bicycle.setGear(3);
+//    console.log(bicycle.gear);
+// --------------------------------------------------------
+// Lesson 15
+// const createPerson = (name, age, gender) => {
+//      return {
+//        name,
+//        age,
+//        gender
+//      };
+//    };
+// --------------------------------------------------------
+// Lesson 14
+// const result = {
+//      success: ["max-length", "no-amd", "prefer-arrow-functions"],
+//      failure: ["no-var", "var-on-top", "linebreak"],
+//      skipped: ["no-extra-semi", "no-dup-keys"]
+//    };
+//    function makeList(arr) {
+//   
+//      const failureItems = [];
+//      for(let i = 0; i < arr.length; i++) {
+//           failureItems.push(`<li class="text-warning">${arr[i]}</li>`)
+//      }
+//      return failureItems;
+//    }
+   
+//    const failuresList = makeList(result.failure);
+// --------------------------------------------------------
+// Lesson 13
+// const stats = {
+//      max: 56.78,
+//      standard_deviation: 4.34,
+//      median: 34.54,
+//      mode: 23.87,
+//      min: -0.75,
+//      average: 35.85
+//    };
+//    const half = ({max, min}) => {
+//      return (max+min) / 2
+//    }
+// --------------------------------------------------------
+// Lesson 12
+// const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// function removeFirstTwo(list) {
+//   // change code below this line
+//   const [,,...arr] = list;
+//   // change code above this line
+//   return arr;
+// }
+// const arr = removeFirstTwo(source);
+// --------------------------------------------------------
+// Lesson 11
+// let a = 8, b = 6;
+// [a,b] = [b, a]
+// --------------------------------------------------------
 // Lesson 10
 // const LOCAL_FORECAST = {
 //      yesterday: { low: 61, high: 75 },
