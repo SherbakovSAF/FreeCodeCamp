6259b02c45f82258bf94168b855ad0e26fb85a8d
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-07 23:52:22 +0300

    TestMethod Lesson 8-10 complite

--- JS/TestMethod.js
+++ JS/TestMethod.js
@@ -1,5 +1,19 @@
+// Lesson 10
+// let quoteSample = "Blueberry 3.141592653s are delicious.";
+// let myRegex = /[2-6h-s]/gi; // Change this line
+// let result = quoteSample.match(myRegex); // Change this line
+// --------------------------------------------------------
+// Lesson 9
+// let quoteSample = "The quick brown fox jumps over the lazy dog.";
+// let alphabetRegex = /[a-z]/ig; // Change this line
+// let result = quoteSample.match(alphabetRegex); // Change this line
+// console.log(result)
+// --------------------------------------------------------
 // Lesson 8
-
+// let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+// let vowelRegex = /[aeiou]/ig; // Change this line
+// let result = quoteSample.match(vowelRegex); 
+// console.log(result)
 // --------------------------------------------------------
 // Lesson 7
 // let exampleStr = "Let's have fun with regular expressions!";
