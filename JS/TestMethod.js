6cc392105e30e279769153826fb81a5ea1c4e676
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-08 22:19:37 +0300

    TestMethod Lesson 16 complite

--- JS/TestMethod.js
+++ JS/TestMethod.js
@@ -1,3 +1,9 @@
+// Lesson 16
+let rickyAndCal = "Cal and Ricky both like racing.";
+let calRegex = /^Cal/; // Change this line
+let result = calRegex.test(rickyAndCal);
+console.log(result)
+// --------------------------------------------------------
 // Lesson 15
 // let reCriminals = /C+/; // Change this line
 // --------------------------------------------------------
