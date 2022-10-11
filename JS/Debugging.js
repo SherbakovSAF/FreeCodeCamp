193379f2e006a6df0ec5ff03d93012f66e4f42a0
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-11 22:24:56 +0300

    Debugging Lesson 4-12 complite

--- JS/Debugging.js
+++ JS/Debugging.js
@@ -1,5 +1,82 @@
-// Lesson 5
+// Lesson 12
+// function myFunc() {
+//      for (let i = 1; i <= 4; i += 2) {
+//        console.log("Still going!");
+//      }
+//    }
+// --------------------------------------------------------
+// Lesson 11
+// function zeroArray(m, n) {
+//      let newArray = [];
+     
+//      for (let i = 0; i < m; i++) {
+//        let row = [];
+//        for (let j = 0; j < n; j++) {
+//          row.push(0)
+//        }
+//        newArray.push(row);
+//      }
+//      return newArray;
+//    }
+   
+//    let matrix = zeroArray(3, 2);
+//    console.log(matrix);
+   
+// --------------------------------------------------------
+// Lesson 10
+// function countToFive() {
+//      let firstFive = "12345";
+//      let len = firstFive.length;
+//      // Only change code below this line
+//      for (let i = 0; i < len; i++) {
+//      // Only change code above this line
+//        console.log(firstFive[i]);
+//      }
+//    }
+   
+//    countToFive();
+// --------------------------------------------------------
+// Lesson 9
+// function raiseToPower(b, e) {
+//      return Math.pow(b, e);
+//    }
+   
+//    let base = 2;
+//    let exp = 3;
+//    let power = raiseToPower(base,exp );
+//    console.log(power);
+// --------------------------------------------------------
+// Lesson 8
+// function getNine() {
+//      let x = 6;
+//      let y = 3;
+//      return x + y;
+//    }
+   
+//    let result = getNine();
+//    console.log(result);
+// --------------------------------------------------------
+// Lesson 7
+// let x = 7;
+// let y = 9;
+// let result = "to come";
 
+// if(x == y) {
+//   result = "Equal!";
+// } else {
+//   result = "Not equal!";
+// }
+
+// console.log(result);
+// --------------------------------------------------------
+// Lesson 6
+// let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+// console.log(innerHtml);
+// --------------------------------------------------------
+// Lesson 5
+// let myArray = [1, 2, 3];
+// let arraySum = myArray.reduce((previous, current) =>  previous + current);
+// console.log(`Sum of array values is: ${arraySum}`);
 // --------------------------------------------------------
 // Lesson 4
 // let receivables = 10;
