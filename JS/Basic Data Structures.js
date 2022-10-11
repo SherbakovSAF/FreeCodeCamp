39109782aa3fb08f7118c9e8a231ba87b216be54
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-12 01:03:25 +0300

    Basic Data Structures Lesson 1-4 complite

---
+++ JS/Basic Data Structures.js
@@ -0,0 +1,24 @@
+// Lesson 4
+// function popShift(arr) {
+//      let popped = arr.pop(); // Change this line
+//      let shifted = arr.shift(); // Change this line
+//      return [shifted, popped];
+//    }
+//    console.log(popShift(['challenge', 'is', 'not', 'complete']));
+// --------------------------------------------------------
+// Lesson 3
+// function mixedNumbers(arr) {
+//      arr.unshift('I', 2, 'three')
+//      arr.push(7, 'VIII', 9)
+//      return arr;
+//    }
+//    console.log(mixedNumbers(['IV', 5, 'six']));
+// --------------------------------------------------------
+// Lesson 2
+// let myArray = ["a", "b", "c", "d"];
+// myArray[1] = 'fdf'
+// console.log(myArray);
+// --------------------------------------------------------
+// Lesson 1
+// let yourArray = ["fdsf", true, 32 , 23, 23]; // Change this line
+// --------------------------------------------------------
