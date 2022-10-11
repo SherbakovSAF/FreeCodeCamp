d48f439ea8189d0e8615eb8bcaed1e1c6d3736ca
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-11 22:02:17 +0300

    Debugging Lesson 3 complite

--- JS/Debugging.js
+++ JS/Debugging.js
@@ -1,3 +1,14 @@
+// Lesson 4
+
+// --------------------------------------------------------
+// Lesson 3
+// let seven = 7;
+// let three = "3";
+// console.log(seven + three);
+// // Only change code below this line
+// console.log(typeof seven)
+// console.log(typeof three)
+// --------------------------------------------------------
 // Lesson 2
 // let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
 // console.log(output)
