c4f3543d2a782515fcf05f9ba4f0e6c98aa9d78c
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-13 00:33:13 +0300

    Basic Algorithm Scripting Lesson 1 complite

---
+++ JS/BasicAlgorithmScripting.js
@@ -0,0 +1,8 @@
+// Lesson 1
+function convertCtoF(celsius) {
+     let fahrenheit = 5/9 * (celsius - 32);
+     return fahrenheit;
+   }
+   
+console.log(convertCtoF(30))
+// --------------------------------------------------------
