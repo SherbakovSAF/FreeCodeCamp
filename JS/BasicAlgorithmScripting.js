8c288e3d1edd0ca4fc594f2411d291d09215869a
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-13 01:06:34 +0300

    Basic Algorithm Scripting Lesson 2-4 complite

--- JS/BasicAlgorithmScripting.js
+++ JS/BasicAlgorithmScripting.js
@@ -1,8 +1,52 @@
-// Lesson 1
-function convertCtoF(celsius) {
-     let fahrenheit = 5/9 * (celsius - 32);
-     return fahrenheit;
-   }
+// Lesson 4
+// function findLongestWordLength(str) {
+//      let count = 0;
+//      let stroke = str.split(" ")
+
+//      for (let i = 0; i < stroke.length; i) {
+//           if (stroke[i].length > count) {
+//                count = stroke[i].length
+//           } else {
+//                i++
+//           }
+//      }
+//      return count;
+//    }
    
-console.log(convertCtoF(30))
+//    console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"))
+// --------------------------------------------------------
+// Lesson 3
+// function factorialize(num) {
+//      let factorial = 1;
+//      for (let i = 1; i <= num; i++){
+//           factorial = factorial * i
+//      }
+//      return factorial;
+//    }
+//    console.log(factorialize(5));
+// --------------------------------------------------------
+// Lesson 2
+// function reverseString(str) {
+//      let arr = [];
+//      let stroke = '';
+//      for(let i = 0; i < str.length; i++){
+//           arr.unshift(str[i])
+          
+//      }
+//      for(let i = 0; i < arr.length; i++){
+//           stroke = stroke + arr[i]
+//      }
+//      return stroke;
+//    }
+   
+// console.log(reverseString("hello"));
+// Да, я знаю что можно сделать код в разы меньше помощью join и splite, но на codeCamp ещё это не проходили
+// --------------------------------------------------------
+// Lesson 1
+// function convertCtoF(celsius) {
+//     let fahrenheit = celsius * (9 / 5) + 32;
+//     return fahrenheit;
+// }
+   
+// console.log(convertCtoF(30))
 // --------------------------------------------------------
