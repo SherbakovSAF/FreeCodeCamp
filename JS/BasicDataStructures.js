32bc936b335a8db8ff4857e0c0a23e48502494d3
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-12 17:08:28 +0300

    Basic Data Structures Lesson 9-12 complite

--- JS/BasicDataStructures.js
+++ JS/BasicDataStructures.js
@@ -1,5 +1,59 @@
-// Lesson 8
+// Lesson 12
+// let myNestedArray = [
+//   // Only change code below this line
+//   ['unshift', false, 1, 2, 3, 'complex', 'nested', ['deep',['deeper',['deepest']]]],
+//   ['loop', 'shift', 6, 7, 1000, 'method'],
+//   ['concat', false, true, 'spread', 'array'],
+//   ['mutate', 1327.98, 'splice', 'slice', 'push'],
+//   ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
+//   // Only change code above this line
+// ];
+// --------------------------------------------------------
+// Lesson 11
+// function filteredArray(arr, elem) {
+//   let newArr = [];
+//   for(let i = 0; i < arr.length; i){
+//     if (arr[i].indexOf(elem) == -1) {
+//       newArr.push(arr[i])
+//       i++
+//     } else {
+//       newArr = newArr
+//       i++
+//     };
+//   }
+//   return newArr;
+// }
 
+// console.log(filteredArray([[1, 2, 3], [3, 2, 1], [2, 4, 6]], 3))
+// --------------------------------------------------------
+// Lesson 10
+// function quickCheck(arr, elem) {
+//   if (arr.indexOf(elem)>=0){
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+// console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+// --------------------------------------------------------
+// Lesson 9
+// function spreadOut() {
+//   let fragment = ['to', 'code'];
+//   let sentence = ['learning', ...fragment, 'is', 'fun']; // Change this line
+//   return sentence;
+// }
+// console.log(spreadOut());
+// --------------------------------------------------------
+// Lesson 8
+// function copyMachine(arr, num) {
+//   let newArr = [];
+//   while (num >= 1) {
+//     newArr.push([...arr])
+//     num--;
+//   }
+//   return newArr;
+// }
+// console.log(copyMachine([true, false, true], 2));
 // --------------------------------------------------------
 // Lesson 7
 // function forecast(arr) {
