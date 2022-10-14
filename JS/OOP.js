7e2ef1466e409951502de3cbe9c10a12386b3620
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-15 01:41:44 +0300

    OOP Lesson 1-5 complite

---
+++ JS/OOP.js
@@ -0,0 +1,41 @@
+// Lesson 5
+// function Dog() {
+//      this.name = "Albert";
+//      this.color = "blue";
+//      this.numLegs = 2;
+//    }
+// --------------------------------------------------------
+// Lesson 4
+// let dog = {
+//      name: "Spot",
+//      numLegs: 4,
+//      sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
+//    };
+   
+//    dog.sayLegs();
+// --------------------------------------------------------
+// Lesson 3
+// let dog = {
+//      name: "Spot",
+//      numLegs: 4,
+//      sayLegs: function() {
+//        return `This dog has ${dog.numLegs} legs.`
+//      }
+//    };
+   
+//    dog.sayLegs();
+// --------------------------------------------------------
+// Lesson 2
+// let dog = {
+//      name: "Spot",
+//      numLegs: 4
+//    };
+//    // Only change code below this line
+//    console.log(dog.name, dog.numLegs)
+// --------------------------------------------------------
+// Lesson 1
+// let dog = {
+//      name: 'Boy',
+//      numLegs: 8
+//      };
+// --------------------------------------------------------
