04c80f87d4e0ecd02301bf624677cc02a8dbb6e8
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-16 01:33:12 +0300

    OOP Lesson 15 complite

--- JS/OOP.js
+++ JS/OOP.js
@@ -1,4 +1,14 @@
-
+// Lesson 15
+// function Dog(name) {
+//      this.name = name;
+//    }
+   
+//    let beagle = new Dog("Snoopy");
+   
+//    // Only change code below this line
+   
+//    Dog.prototype.isPrototypeOf(beagle)
+// --------------------------------------------------------
 // Lesson 14
 // function Dog(name) {
 //      this.name = name;
