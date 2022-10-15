0b275e16d6bfed54aa6eb3c005e0f8ad13db6015
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-16 01:28:19 +0300

    OOP Lesson 12 complite

--- JS/OOP.js
+++ JS/OOP.js
@@ -1,4 +1,18 @@
 
+// Lesson 12
+// function Dog(name) {
+//      this.name = name;
+//    }
+   
+//    // Only change code below this line
+//    function joinDogFraternity(candidate) {
+//      if (candidate.constructor == Dog) {
+//        return true
+//      } else {
+//        return false
+//      }
+//    }
+// --------------------------------------------------------
 // Lesson 11
 // function Dog(name) {
 //      this.name = name;
