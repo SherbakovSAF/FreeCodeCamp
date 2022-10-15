b8127dbcaf4f7dd97e32612afa31f6925fff7700
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-16 01:31:42 +0300

    OOP Lesson 14 complite

--- JS/OOP.js
+++ JS/OOP.js
@@ -1,4 +1,21 @@
 
+// Lesson 14
+// function Dog(name) {
+//      this.name = name;
+//    }
+   
+//    // Only change code below this line
+//    Dog.prototype = {
+//      constructor: Dog,
+//      numLegs: 4,
+//      eat: function() {
+//        console.log("nom nom nom");
+//      },
+//      describe: function() {
+//        console.log("My name is " + this.name);
+//      }
+//    };
+// --------------------------------------------------------
 // Lesson 13
 // function Dog(name) {
 //      this.name = name;
