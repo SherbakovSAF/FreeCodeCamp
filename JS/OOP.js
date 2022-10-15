46d269bf33eda895ef70bc901a9c6ce47dff4ba6
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-16 01:26:01 +0300

    OOP Lesson 11 complite

--- JS/OOP.js
+++ JS/OOP.js
@@ -1,3 +1,29 @@
+
+// Lesson 11
+// function Dog(name) {
+//      this.name = name;
+//    }
+   
+//    Dog.prototype.numLegs = 4;
+   
+//    let beagle = new Dog("Snoopy");
+   
+//    let ownProps = [];
+//    let prototypeProps = [];
+   
+//    // Only change code below this line
+   
+//    for (let property in beagle) {
+//      if(beagle.hasOwnProperty(property)){
+//        ownProps.push(property)
+//      } else {
+//        prototypeProps.push(property)
+//      }
+//    }
+   
+//    console.log(ownProps);
+//    console.log(prototypeProps);
+// --------------------------------------------------------
 // Lesson 10
 // function Dog(name) {
 //      this.name = name;
