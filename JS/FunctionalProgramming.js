0e0d3ce823bff3449b12fc46a81aff56ec9541b4
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-19 23:28:09 +0300

    FunctionalProgramming Lesson 17 complite

--- JS/FunctionalProgramming.js
+++ JS/FunctionalProgramming.js
@@ -1,3 +1,15 @@
+// Lesson 17
+// function alphabeticalOrder(arr) {
+//    // Only change code below this line
+ 
+//    return arr.sort(function(a, b) {
+//      return a === b ? 0 : a > b ? 1 : -1;
+//    });
+//    // Only change code above this line
+//  }
+ 
+//  alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
+// --------------------------------------------------------
 // Lesson 16
 // const squareList = arr => {
 //    // Only change code below this line
