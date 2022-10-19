fe74fea9496f1cb70fd54faf50c97c45cad7d344
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-19 23:31:34 +0300

    FunctionalProgramming Lesson 18 complite

--- JS/FunctionalProgramming.js
+++ JS/FunctionalProgramming.js
@@ -1,3 +1,17 @@
+// Lesson 18
+// const globalArray = [5, 6, 3, 2, 9];
+
+// function nonMutatingSort(arr) {
+//   // Only change code below this line
+// return arr.slice(0).sort(function(a,b){
+//   return a-b
+// })
+
+//   // Only change code above this line
+// }
+
+// console.log(nonMutatingSort(globalArray))
+// --------------------------------------------------------
 // Lesson 17
 // function alphabeticalOrder(arr) {
 //    // Only change code below this line
