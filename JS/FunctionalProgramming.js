7374b7c7e9913355ca516275f2a36983267a4194
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-20 23:22:35 +0300

    FunctionalProgramming Lesson 20 complite

--- JS/FunctionalProgramming.js
+++ JS/FunctionalProgramming.js
@@ -1,3 +1,14 @@
+// Lesson 20
+// function sentensify(str) {
+//    // Only change code below this line
+//  return str.split(/\W/).join(' ')
+ 
+//    // Only change code above this line
+//  }
+ 
+//  ;
+//  console.log(sentensify("May-the-force-be-with-you"))
+// --------------------------------------------------------
 // Lesson 19
 // function splitify(str) {
 //    // Only change code below this line
