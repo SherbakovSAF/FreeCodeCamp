94bd5f43c58b1445d3949c1186e2d1f2c954982a
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-20 23:33:11 +0300

    FunctionalProgramming Lesson 24 complite

--- JS/FunctionalProgramming.js
+++ JS/FunctionalProgramming.js
@@ -1,3 +1,18 @@
+// Lesson 24
+// function add(x) {
+//    // Only change code below this line
+//  return function(y){
+//    return function(z) {
+//  return x + y + z
+//    }
+   
+//  }
+ 
+//    // Only change code above this line
+//  }
+ 
+//  add(10)(20)(30);
+// --------------------------------------------------------
 // Lesson 23
 // function checkPositive(arr) {
 //    // Only change code below this line
