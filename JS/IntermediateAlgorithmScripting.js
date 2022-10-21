159492fb2a23d10b0b268c22a27cacd9cd23ba1a
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-21 15:59:16 +0300

    IntermediateAlgorithmScripting Lesson 1 complite

---
+++ JS/IntermediateAlgorithmScripting.js
@@ -0,0 +1,20 @@
+// Lesson 1
+// function sumAll(arr) {
+//    let newArray = [];
+//    if (arr[0] - arr[1] < 0) {
+//        for (let i = arr[0]; i <= arr[1]; i++) {
+//            newArray.push(i);
+//        }
+//    } else {
+//        for (let i = arr[0]; i >= arr[1]; i--) {
+//            newArray.push(i);
+//        }
+     
+//    }
+//  return newArray.reduce((prev, item) =>{
+//      return prev + item
+//  })
+
+   
+// }
+// --------------------------------------------------------
