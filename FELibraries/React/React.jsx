fbe5bf37dd578f0ac9993ac10078ca522940f0a9
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-24 20:34:07 +0300

    React Lesson 7-8 complite

--- FELibraries/React/React.jsx
+++ FELibraries/React/React.jsx
@@ -1,4 +1,28 @@
 
+// Lesson 8
+// class MyComponent extends React.Component {
+//     constructor(props) {
+//         super(props);
+//     }
+//     render() {
+//         // Change code below this line
+//         return (
+//             <div>
+//                 <h1>Hello React!</h1>
+//             </div>
+//         );
+
+//         // Change code above this line
+//     }
+// }
+// --------------------------------------------------------
+// Lesson 7
+// const MyComponent = function() {
+//    return (
+//      <div>авыавы</div>
+//    )
+//    }
+// --------------------------------------------------------
 // Lesson 6
 // const JSX = (
 //      <div>
