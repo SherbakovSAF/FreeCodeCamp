4feaddb0930fa6b10fda4abc274627ba257b4425
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-24 20:39:39 +0300

    React Lesson 9-10 complite

--- FELibraries/React/React.jsx
+++ FELibraries/React/React.jsx
@@ -1,4 +1,71 @@
-
+// Lesson 10
+// const TypesOfFruit = () => {
+//      return (
+//        <div>
+//          <h2>Fruits:</h2>
+//          <ul>
+//            <li>Apples</li>
+//            <li>Blueberries</li>
+//            <li>Strawberries</li>
+//            <li>Bananas</li>
+//          </ul>
+//        </div>
+//      );
+//    };
+   
+//    const Fruits = () => {
+//      return (
+//        <div>
+//          { /* Change code below this line */ }
+//            <TypesOfFruit />
+//          { /* Change code above this line */ }
+//        </div>
+//      );
+//    };
+   
+//    class TypesOfFood extends React.Component {
+//      constructor(props) {
+//        super(props);
+//      }
+   
+//      render() {
+//        return (
+//          <div>
+//            <h1>Types of Food:</h1>
+//            { /* Change code below this line */ }
+//            <Fruits />
+//            { /* Change code above this line */ }
+//          </div>
+//        );
+//      }
+//    };
+// --------------------------------------------------------
+// Lesson 9
+// const ChildComponent = () => {
+//      return (
+//        <div>
+//          <p>I am the child</p>
+//        </div>
+//      );
+//    };
+   
+//    class ParentComponent extends React.Component {
+//      constructor(props) {
+//        super(props);
+//      }
+//      render() {
+//        return (
+//          <div>
+//            <h1>I am the parent</h1>
+//            { /* Change code below this line */ }
+   
+//    <ChildComponent/>
+//            { /* Change code above this line */ }
+//          </div>
+//        );
+//      }
+//    };
+// --------------------------------------------------------
 // Lesson 8
 // class MyComponent extends React.Component {
 //     constructor(props) {
