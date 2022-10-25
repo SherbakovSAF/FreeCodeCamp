8b7eedc849b042eddf6d5753c865dc048583f14f
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-25 23:18:07 +0300

    React Lesson 23-24 complite

--- FELibraries/React/React.jsx
+++ FELibraries/React/React.jsx
@@ -1,23 +1,69 @@
-
-
+// Lesson 24
+// class MyComponent extends React.Component {
+//      constructor(props) {
+//        super(props);
+//        this.state = {
+//          name: 'Initial State'
+//        };
+//        this.handleClick = this.handleClick.bind(this);
+//      }
+//      handleClick() {
+//        // Change code below this line
+//    this.setState({
+//      name: "React Rocks!"
+//    })
+//        // Change code above this line
+//      }
+//      render() {
+//        return (
+//          <div>
+//            <button onClick={this.handleClick}>Click Me</button>
+//            <h1>{this.state.name}</h1>
+//          </div>
+//        );
+//      }
+//    };
+// --------------------------------------------------------
+// Lesson 23
+// class MyComponent extends React.Component {
+//      constructor(props) {
+//        super(props);
+//        this.state = {
+//          name: 'freeCodeCamp'
+//        }
+//      }
+//      render() {
+//        // Change code below this line
+//    const name = this.state.name
+//        // Change code above this line
+//        return (
+//          <div>
+//            { /* Change code below this line */ }
+//    <h1>{name}</h1>
+//            { /* Change code above this line */ }
+//          </div>
+//        );
+//      }
+//    };
+// --------------------------------------------------------
 // Lesson 22
-class MyComponent extends React.Component {
-     constructor(props) {
-       super(props);
-       this.state = {
-         name: 'freeCodeCamp'
-       }
-     }
-     render() {
-       return (
-         <div>
-           { /* Change code below this line */ }
-           <h1>{this.state.name}</h1>
-           { /* Change code above this line */ }
-         </div>
-       );
-     }
-   };
+// class MyComponent extends React.Component {
+//      constructor(props) {
+//        super(props);
+//        this.state = {
+//          name: 'freeCodeCamp'
+//        }
+//      }
+//      render() {
+//        return (
+//          <div>
+//            { /* Change code below this line */ }
+//            <h1>{this.state.name}</h1>
+//            { /* Change code above this line */ }
+//          </div>
+//        );
+//      }
+//    };
 // --------------------------------------------------------
 // Lesson 21
 // class StatefulComponent extends React.Component {
