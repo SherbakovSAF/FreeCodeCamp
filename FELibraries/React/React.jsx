454570062e973688e75e42feb9e99f984158dfb3
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-26 21:51:04 +0300

    React Lesson 25-26 complite
    Пока что я делаю паузу, так как уже мне сложно понять что происходит

--- FELibraries/React/React.jsx
+++ FELibraries/React/React.jsx
@@ -1,3 +1,68 @@
+// Lesson 26
+// class MyComponent extends React.Component {
+//    constructor(props) {
+//      super(props);
+//      this.state = {
+//        visibility: false
+//      };
+//      // Change code below this line
+//      this.toggleVisibility = this.toggleVisibility.bind(this);
+//      // Change code above this line
+//    }
+//    // Change code below this line
+//  toggleVisibility() {
+//      this.setState((state, props) => ({
+//        visibility: !state.visibility
+//      }))
+//    }
+ 
+//    // Change code above this line
+//    render() {
+//      if (this.state.visibility) {
+//        return (
+//          <div>
+//            <button onClick={this.toggleVisibility}>Click Me</button>
+//            <h1>Now you see me!</h1>
+//          </div>
+//        );
+//      } else {
+//        return (
+//          <div>
+//            <button onClick={this.toggleVisibility}>Click Me</button>
+//          </div>
+//        );
+//      }
+//    }
+//  }
+ // --------------------------------------------------------
+// Lesson 25
+// class MyComponent extends React.Component {
+//    constructor(props) {
+//      super(props);
+//      this.state = {
+//        text: "Hello"
+//      };
+//      // Change code below this line
+//    this.handleClick = this.handleClick.bind(this)
+//      // Change code above this line
+//    }
+//    handleClick() {
+//      this.setState({
+//        text: "You clicked!"
+//      });
+//    }
+//    render() {
+//      return (
+//        <div>
+//          { /* Change code below this line */ }
+//          <button onClick={this.handleClick}>Click Me</button>
+//          { /* Change code above this line */ }
+//          <h1>{this.state.text}</h1>
+//        </div>
+//      );
+//    }
+//  };
+ // --------------------------------------------------------
 // Lesson 24
 // class MyComponent extends React.Component {
 //      constructor(props) {
