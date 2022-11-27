62ea2d9d399b440605142d41a2677aef59df20cf
SherbakovSAF <sherbakovsaf@gmail.com>
2022-11-27 13:12:35 +0300

    React Lesson 33 complite

--- FELibraries/React/React.jsx
+++ FELibraries/React/React.jsx
@@ -1,3 +1,29 @@
+// Lesson 33
+// class MyComponent extends React.Component {
+//    constructor(props) {
+//      super(props);
+//      this.state = {
+//        activeUsers: null
+//      };
+//    }
+//    componentDidMount() {
+//      setTimeout(() => {
+//        this.setState({
+//          activeUsers: 1273
+//        });
+//      }, 2500);
+//    }
+//    render() {
+//      return (
+//        <div>
+//          {/* Change code below this line */}
+//          <h1>Active Users: {this.state.activeUsers}</h1>
+//          {/* Change code above this line */}
+//        </div>
+//      );
+//    }
+//  }
+
 // Lesson 32
 // class MyComponent extends React.Component {
 //    constructor(props) {
