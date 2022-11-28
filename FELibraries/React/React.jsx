8dee21ff35109ef44e37ab55f1ce10059a58b9c7
SherbakovSAF <sherbakovsaf@gmail.com>
2022-11-28 18:06:17 +0300

    React Lesson 37 complite

--- FELibraries/React/React.jsx
+++ FELibraries/React/React.jsx
@@ -1,3 +1,15 @@
+// Lesson 37
+// const styles = {color: "purple", fontSize: 40, border: "2px solid purple"}
+// class Colorful extends React.Component {
+//   render() {
+//     // Change code below this line
+//     return (
+//       <div style={styles}>Style Me!</div>
+//     );
+//     // Change code above this line
+//   }
+// };
+
 // Lesson 36
 // class Colorful extends React.Component {
 //    render() {
