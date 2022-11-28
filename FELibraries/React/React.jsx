36f11a826d0e8810451d19ff34b09cb0f3ebaad7
SherbakovSAF <sherbakovsaf@gmail.com>
2022-11-28 18:06:04 +0300

    React Lesson 36 complite

--- FELibraries/React/React.jsx
+++ FELibraries/React/React.jsx
@@ -1,4 +1,11 @@
-
+// Lesson 36
+// class Colorful extends React.Component {
+//    render() {
+//      return (
+//        <div style={{color: "red", fontSize: 72}}>Big Red</div>
+//      );
+//    }
+//  };
 
 // Lesson 35
 // class OnlyEvens extends React.Component {
