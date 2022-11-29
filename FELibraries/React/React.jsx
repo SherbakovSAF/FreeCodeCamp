42281b9e85b4cd739e22b0c198078a16e952249a
SherbakovSAF <sherbakovsaf@gmail.com>
2022-11-29 22:11:53 +0300

    React Lesson 46 complite

--- FELibraries/React/React.jsx
+++ FELibraries/React/React.jsx
@@ -1,3 +1,54 @@
+// Lesson 46
+// class MyComponent extends React.Component {
+//    constructor(props) {
+//      super(props);
+//      this.state = {
+//        users: [
+//          {
+//            username: 'Jeff',
+//            online: true
+//          },
+//          {
+//            username: 'Alan',
+//            online: false
+//          },
+//          {
+//            username: 'Mary',
+//            online: true
+//          },
+//          {
+//            username: 'Jim',
+//            online: false
+//          },
+//          {
+//            username: 'Sara',
+//            online: true
+//          },
+//          {
+//            username: 'Laura',
+//            online: true
+//          }
+//        ]
+//      };
+//    }
+//    render() {
+//       const usersOnline = this.state.users.filter(user => user.online === true); // change code here
+//      const renderOnline = usersOnline.map(user => <li key={user.username}>{user.username}</li>); // change code here
+//      return (
+//        <div>
+//          <h1>Current Online Users:</h1>
+         
+//          <ul>{renderOnline}</ul>
+//        </div>
+//      );
+//    }
+//  }
+
+
+// Не знаю почему, но моё идентичное решение он отказывается принимать, хотя оно точно такое же
+// const usersOnline = this.state.users.filter(e => e.online == true); // Change this line
+// const renderOnline = usersOnline.map(e => <li key={e.username}>{e}</li>); // Change this line
+
 // Lesson 45
 // const frontEndFrameworks = [
 //    'React',
