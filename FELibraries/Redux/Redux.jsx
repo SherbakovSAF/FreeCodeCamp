9e299f5f3eb64d32bcaeb28cccf290ee1e9971fe
SherbakovSAF <sherbakovsaf@gmail.com>
2022-11-30 05:57:43 +0300

    Redux Lesson 2 complite

--- FELibraries/Redux/Redux.jsx
+++ FELibraries/Redux/Redux.jsx
@@ -1,3 +1,9 @@
+// Lesson 2
+// const store = Redux.createStore(
+//    (state = 5) => state
+//  );
+//  const currentState = store.getState()
+
 // Lesson 1
 // const reducer = (state = 5) => {
 //    return state;
