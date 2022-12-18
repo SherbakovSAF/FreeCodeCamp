d3d4582d6009dc3ad7ece8facbd326b9e315991f
SherbakovSAF <sherbakovsaf@gmail.com>
2022-12-18 18:53:15 +0300

    Redux Lesson 14 complite

--- FELibraries/Redux/Redux.jsx
+++ FELibraries/Redux/Redux.jsx
@@ -1,3 +1,33 @@
+// Lesson 14
+// const ADD_TO_DO = 'ADD_TO_DO';
+
+// // A list of strings representing tasks to do:
+// const todos = [
+//   'Go to the store',
+//   'Clean the house',
+//   'Cook dinner',
+//   'Learn to code',
+// ];
+
+// const immutableReducer = (state = todos, action) => {
+//   switch(action.type) {
+//     case ADD_TO_DO:
+//       return [...state, action.todo]
+//     default:
+//       return state;
+//   }
+// };
+
+// const addToDo = (todo) => {
+//   return {
+//     type: ADD_TO_DO,
+//     todo
+//   }
+// }
+
+// const store = Redux.createStore(immutableReducer);
+
+
 // Lesson 13
 // const INCREMENT = 'INCREMENT';
 // const DECREMENT = 'DECREMENT';
