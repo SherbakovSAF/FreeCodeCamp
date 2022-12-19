b6204e65f6c7e99aa390b00b5bcad31ab2d09666
SherbakovSAF <sherbakovsaf@gmail.com>
2022-12-19 22:41:00 +0300

    Redux Lesson 15 complite

--- FELibraries/Redux/Redux.jsx
+++ FELibraries/Redux/Redux.jsx
@@ -1,3 +1,23 @@
+// Lesson 15
+// const immutableReducer = (state = ['Do not mutate state!'], action) => {
+//      switch(action.type) {
+//        case 'ADD_TO_DO':
+//          // Don't mutate state here or the tests will fail
+//          return [...state, action.todo]
+//        default:
+//          return state;
+//      }
+//    };
+   
+//    const addToDo = (todo) => {
+//      return {
+//        type: 'ADD_TO_DO',
+//        todo
+//      }
+//    }
+   
+//    const store = Redux.createStore(immutableReducer);
+
 // Lesson 14
 // const ADD_TO_DO = 'ADD_TO_DO';
 
