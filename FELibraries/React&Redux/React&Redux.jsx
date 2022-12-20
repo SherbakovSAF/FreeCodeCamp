7cf517a6899d27b106e5a802fe0aaeb96ece2564
SherbakovSAF <sherbakovsaf@gmail.com>
2022-12-20 23:01:46 +0300

    React&Redux Lesson 2 complite

--- FELibraries/React&Redux/React&Redux.jsx
+++ FELibraries/React&Redux/React&Redux.jsx
@@ -1,3 +1,42 @@
+// Lesson 2
+// class DisplayMessages extends React.Component {
+//    constructor(props) {
+//      super(props);
+//      this.state = {
+//        input: '',
+//        messages: []
+//      }
+//      this.handleChange = this.handleChange.bind(this);
+//      this.submitMessage = this.submitMessage.bind(this);
+//    }
+//    // Add handleChange() and submitMessage() methods here
+   
+//    handleChange(e) {
+//       this.setState({input: e.target.value})
+//        };
+ 
+//    submitMessage(){
+//     this.setState({
+//       input: "",
+//       messages: [...this.state.messages, this.state.input]})
+//     }
+//    render() {
+     
+//      return (
+//        <div>
+//          <h2>Type in a new Message:</h2>
+//          { /* Render an input, button, and ul below this line */ }
+//          <input value={this.state.input} onChange={this.handleChange}/>
+//          <button onClick={this.submitMessage}>Add message</button>
+//          <ul>
+//          {this.state.messages.map(e => <li>{e}</li>)}
+//          </ul>
+//          { /* Change code above this line */ }
+//        </div>
+//      );
+//    }
+//  };
+
 // Lesson 1
 // class DisplayMessages extends React.Component {
 //    // Change code below this line
