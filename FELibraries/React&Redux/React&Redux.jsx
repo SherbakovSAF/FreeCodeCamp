afb1086273b03b9c69fbbfe74aca86ad517a7e99
SherbakovSAF <sherbakovsaf@gmail.com>
2022-12-20 23:01:52 +0300

    React&Redux Lesson 3 complite

--- FELibraries/React&Redux/React&Redux.jsx
+++ FELibraries/React&Redux/React&Redux.jsx
@@ -1,3 +1,24 @@
+// Lesson 3
+// const ADD = "ADD"
+
+// const addMessage = (message) => {
+//   return {
+//     type: ADD,
+//     message: message,
+//   }
+// }
+
+// const messageReducer = (state = [], action) => {
+//   switch(action.type){
+//     case ADD:
+//       return [...state, action.message]
+//     default: 
+//       return state
+//   } 
+// }
+
+// const store = Redux.createStore(messageReducer)
+
 // Lesson 2
 // class DisplayMessages extends React.Component {
 //    constructor(props) {
