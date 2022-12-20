871092d9805eff80badb905e22b4195939717396
SherbakovSAF <sherbakovsaf@gmail.com>
2022-12-20 22:22:37 +0300

    React&Redux Lesson 1 complite

---
+++ FELibraries/React&Redux/React&Redux.jsx
@@ -0,0 +1,15 @@
+// Lesson 1
+// class DisplayMessages extends React.Component {
+//    // Change code below this line
+//    constructor(props){
+//      super(props);
+//      this.state ={
+//        messages: [],
+//        input: "",
+//      }
+//    }
+//    // Change code above this line
+//    render() {
+//      return <div />
+//    }
+//  };
